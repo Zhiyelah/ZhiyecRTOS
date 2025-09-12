84e43304b703708f65c1679c5fca488e52f879df
Zhiyelah <Zhiyelah@outlook.com>
2025-09-12 23:15:07 +0800

    add annotation

--- Utility/Printf.h
+++ Utility/Printf.h
@@ -1,3 +1,10 @@
+/**
+ * @file Printf.h
+ * @author Zhiyelah
+ * @brief 格式化打印
+ * @note 无需禁用半主机模式, 且支持多线程环境的打印输出
+ */
+
 #ifndef _Printf_h
 #define _Printf_h
 
@@ -8,6 +15,7 @@ void Printf_setOutput(void (*output)(char));
 
 /**
  * @brief 格式化打印
+ * @note 线程安全
  */
 void Printf(const char *format, ...);
 
